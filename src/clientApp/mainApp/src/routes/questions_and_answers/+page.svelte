018466a3a1f77c5d51c5b8dcb0b33cdc67d80fee
Dmitry Ivahno <d@ivahno.com>
2023-11-10 21:40:15 -0800

    recommendation section

--- src/clientApp/mainApp/src/routes/questions_and_answers/+page.svelte
+++ src/clientApp/mainApp/src/routes/questions_and_answers/+page.svelte
@@ -48,9 +48,9 @@
             index: counter,
             q: element.Question,
             a: element.Answer,
-            r1: undefined,
-            r2: undefined,
-            r3: undefined,
+            r1: '',
+            r2: '',
+            r3: '',
           };
           newList.push(newQandA);
           counter++;
@@ -180,148 +180,17 @@
               </tr>
             </thead>
             <tbody>
-              <!-- row 1 -->
+              {#each Array.from($projectData.qAndA) as qAndA}
               <tr>
-                <th>1</th>
-                <td
-                  >Explain the concept of event-driven programming in Node.js.</td
-                >
-                <td
-                  >Node.js is built around an event-driven architecture, where
-                  certain kinds of objects (called "emitters") emit named events
-                  that cause Function objects ("listeners") to be called.</td
-                >
-                <td>Event-driven architecture</td>
-                <td>Emitters emit events</td>
-                <td>Triggers listeners to be called</td>
+                <th>{qAndA.index}</th>
+                <td>{qAndA.q}</td>
+                <td>{qAndA.a}</td>
+                <td>{qAndA.r1}</td>
+                <td>{qAndA.r2}</td>
+                <td>{qAndA.r3}</td>
               </tr>
-              <!-- row 2 -->
-              <tr>
-                <th>2</th>
-                <td>What is the Node.js event loop and how does it work?</td>
-                <td
-                  >The event loop is what allows Node.js to perform non-blocking
-                  I/O operations, despite JavaScript being single-threaded. It
-                  handles all asynchronous callbacks in a loop.</td
-                >
-                <td>Non-blocking I/O</td>
-                <td>Single-threaded nature</td>
-                <td>Asynchronous callbacks</td>
-              </tr>
-              <!-- row 3 -->
-              <tr>
-                <th>3</th>
-                <td>What are streams in Node.js?</td>
-                <td
-                  >Streams are collections of data that might not be available
-                  all at once and don't need to fit in memory. This makes
-                  streams ideal for reading or writing large amounts of data.</td
-                >
-                <td>Handling large data</td>
-                <td>Efficient data processing</td>
-                <td>Readable and writable</td>
-              </tr>
-              <!-- row 4 -->
-              <tr>
-                <th>4</th>
-                <td>How does Node.js handle child threads?</td>
-                <td
-                  >Node.js, primarily being single-threaded, can spawn child
-                  processes to handle parallel processing of tasks using the
-                  'child_process' module.</td
-                >
-                <td>Child processes</td>
-                <td>Parallel processing</td>
-                <td>'child_process' module</td>
-              </tr>
-              <!-- row 5 -->
-              <tr>
-                <th>5</th>
-                <td>Explain the use of middleware in Express.js.</td>
-                <td
-                  >Middleware functions are functions that have access to the
-                  request object (req), the response object (res), and the next
-                  middleware function in the application’s request-response
-                  cycle.</td
-                >
-                <td>Request-response cycle</td>
-                <td>Function access to req and res</td>
-                <td>Next middleware function</td>
-              </tr>
-              <!-- row 6 -->
-              <tr>
-                <th>6</th>
-                <td
-                  >What is the difference between process.nextTick() and
-                  setImmediate()?</td
-                >
-                <td
-                  >The process.nextTick() function defers the execution of an
-                  action till the next pass around the event loop.
-                  setImmediate() is designed to execute a script once the
-                  current poll phase completes.</td
-                >
-                <td>Event loop phases</td>
-                <td>process.nextTick() usage</td>
-                <td>setImmediate() usage</td>
-              </tr>
-              <!-- row 7 -->
-              <tr>
-                <th>7</th>
-                <td
-                  >How do you ensure that a Node.js process runs continuously?</td
-                >
-                <td
-                  >Node.js processes can be kept running continuously by using
-                  process managers like PM2, which help in managing and keeping
-                  the Node.js processes alive.</td
-                >
-                <td>Process managers</td>
-                <td>PM2 usage</td>
-                <td>Continuous running</td>
-              </tr>
-              <!-- row 8 -->
-              <tr>
-                <th>8</th>
-                <td>What are worker threads in Node.js?</td>
-                <td
-                  >Worker threads are a way to create multiple threads in
-                  Node.js, allowing CPU-intensive tasks to be handled in the
-                  background without blocking the main thread.</td
-                >
-                <td>Multi-threading</td>
-                <td>CPU-intensive tasks</td>
-                <td>Non-blocking processing</td>
-              </tr>
-              <!-- row 9 -->
-              <tr>
-                <th>9</th>
-                <td>Explain the concept of middleware in Node.js.</td>
-                <td
-                  >Middleware in Node.js is a function that has access to the
-                  request object, response object, and the next function in the
-                  application’s request-response cycle. It can execute any code,
-                  modify request and response objects, and end the
-                  request-response cycle.</td
-                >
-                <td>Code execution</td>
-                <td>Modify req/res objects</td>
-                <td>Control flow</td>
-              </tr>
-              <!-- row 10 -->
-              <tr>
-                <th>10</th>
-                <td>What is the purpose of module.exports in Node.js?</td>
-                <td
-                  >module.exports is used to export functions, objects, or
-                  primitive values from a module so they can be used by other
-                  programs with the require() function.</td
-                >
-                <td>Module reusability</td>
-                <td>Exporting modules</td>
-                <td>require() function</td>
-              </tr>
-              <!-- Additional rows omitted for brevity -->
+            {/each}
+              
             </tbody>
           </table>
         </div>
