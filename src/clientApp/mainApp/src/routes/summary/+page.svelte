9c5313c0293a57a759845a773861ea6a6f481837
Dmitry Ivahno <d@ivahno.com>
2023-11-11 17:55:05 -0800

    sync up files with BVAI

--- src/clientApp/mainApp/src/routes/summary/+page.svelte
+++ src/clientApp/mainApp/src/routes/summary/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { writable } from "svelte/store";
-  import { ProjectState } from "../../models/project-data";
+  import { ProjectState, type ProjectData } from "../../models/project-data";
   import { projectData, projectState } from "../../stores/project.store";
   import StepWithContent from "../../components/StepWithContent.svelte";
   import {
@@ -10,6 +10,10 @@
     getSummaryResume,
   } from "../../services/projectStatus";
   import { onMount } from "svelte";
+  import {
+    getFiles,
+    syncTextAndFile,
+  } from "../../services/boostVoiceAi";
 
   let name = "";
   let description = "";
@@ -26,20 +30,44 @@
     $currentStep = $currentStep + 1;
   }
 
-  function saveProjectSummary() {
+  const syncWithBVAI = async (pd: ProjectData) => {
+    const allFiles = await getFiles();
+    await syncTextAndFile(
+      pd.jobDescription,
+      "job_description.txt",
+      allFiles,
+      "fine-tuning",
+      "user-provided"
+    );
+    await syncTextAndFile(
+      pd.resume,
+      "resume.txt",
+      allFiles,
+      "fine-tuning",
+      "user-provided"
+    );
+  };
+
+  const saveProjectSummary = async () => {
+    const summaryChanges = {
+      projectName: name,
+      projectDescription: description,
+      jobDescription: jobDescription,
+      resume: resume,
+      preferences: preferences,
+    };
+
     projectData.update((pd) => {
       return {
         ...pd,
-        projectName: name,
-        projectDescription: description,
-        jobDescription: jobDescription,
-        resume: resume,
-        preferences : preferences,
+        ...summaryChanges,
       };
     });
 
+    await syncWithBVAI({ ...$projectData, ...summaryChanges });
+
     projectState.set(ProjectState.Configuring);
-  }
+  };
 </script>
 
 <div class="card bg-base-100 shadow-xl">
@@ -111,7 +139,7 @@
         </div>
       </div>
     {:else if $currentStep === 3}
-    <h2 class="card-title">Resume</h2>
+      <h2 class="card-title">Resume</h2>
       <div class="form step-one mt-8 items-center flex flex-col">
         <div class="form-control w-full max-w-xs">
           <label class="label" for="summmary_resume">
@@ -126,7 +154,7 @@
         </div>
       </div>
     {:else}
-    <h2 class="card-title">Your preferences</h2>
+      <h2 class="card-title">Your preferences</h2>
       <div class="form step-one mt-8 items-center flex flex-col">
         <div class="form-control w-full max-w-xs">
           <label class="label" for="summmary_preferences">
--- src/clientApp/mainApp/src/services/boostVoiceAi.ts
+++ src/clientApp/mainApp/src/services/boostVoiceAi.ts
@@ -31,12 +31,138 @@ export interface PhonePool {
   isPrivate: boolean;
 }
 
+export interface BVAIFile {
+  id: string;
+  organizationId: string;
+  name: string;
+  purpose: string;
+  description: string;
+  size: number;
+  createdUtc: Date;
+}
+
+export interface FileCreationSettings {
+  File: File;
+  purpose: string;
+  description: string;
+}
+
 const bvaiBaseUrl = "http://localhost:8088/api";
 
 const standardHeaders = {
   "Content-Type": "application/json",
   "BVAI-Key": PUBLIC_BOOST_VOICE_AI_APIKEY,
 };
+
+const uploadHeaders = {
+  "Content-Type": "multipart/form-data",
+  "BVAI-Key": PUBLIC_BOOST_VOICE_AI_APIKEY,
+};
+
+export const syncTextAndFile = async (
+    fileContent: string | undefined,
+    fileName: string,
+    allFiles: BVAIFile[],
+    purpose: string,
+    description: string
+  ) => {
+    let changeHappened = false;
+    const existingFile = allFiles.find((file) => file.name == fileName);
+    if (existingFile) {
+      await deleteFile(existingFile.id);
+      changeHappened = true;
+    }
+
+    if (fileContent) {
+      //add file
+      var fileBlob = new Blob([fileContent ?? ""]);
+      var newFile = new File([fileBlob], fileName);
+
+      await uploadFile({
+        File: newFile,
+        purpose,
+        description,
+      });
+      changeHappened = true;
+    }
+    if (changeHappened){
+        allFiles = await getFiles();
+    }
+  };
+
+export async function uploadFile(
+  fileCreationSettings: FileCreationSettings
+): Promise<BVAIFile> {
+  return new Promise(async (resolve, reject) => {
+    // Create a FormData object
+    const formData = new FormData();
+
+    formData.append("File", fileCreationSettings.File);
+
+    formData.append("purpose", fileCreationSettings.purpose);
+    formData.append("description", fileCreationSettings.description);
+
+    try {
+      const response = await axios.post(
+        `${bvaiBaseUrl}/files/upload`,
+        formData,
+        {
+          headers: uploadHeaders,
+        }
+      );
+
+      if (response.status === 200) {
+        resolve(response.data.data as BVAIFile);
+      } else {
+        console.error("Error: Unable to stream audio.");
+        reject();
+      }
+    } catch (error) {
+      console.error("Error: Unable to stream audio.");
+      reject();
+    }
+  });
+}
+
+export async function deleteFile(fileId: string): Promise<boolean> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await axios.delete(`${bvaiBaseUrl}/files/${fileId}`, {
+        headers: standardHeaders,
+      });
+
+      if (response.status === 200) {
+        resolve(true);
+      } else {
+        console.error("Error: Unable to stream audio.");
+        reject();
+      }
+    } catch (error) {
+      console.error("Error: Unable to stream audio.");
+      reject();
+    }
+  });
+}
+
+export async function getFiles(): Promise<BVAIFile[]> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await axios.get(`${bvaiBaseUrl}/files`, {
+        headers: standardHeaders,
+      });
+
+      if (response.status === 200) {
+        resolve(response.data.data as BVAIFile[]);
+      } else {
+        console.error("Error: Unable to stream audio.");
+        reject();
+      }
+    } catch (error) {
+      console.error("Error: Unable to stream audio.");
+      reject();
+    }
+  });
+}
 
 export async function getAvailablePhonePools(): Promise<PhonePool[]> {
   return new Promise(async (resolve, reject) => {
@@ -63,13 +189,17 @@ export async function createProject(
 ): Promise<ProjectWithSettings> {
   return new Promise(async (resolve, reject) => {
     const requestBody = {
-        ...projectCreationDetails,
-      };
+      ...projectCreationDetails,
+    };
 
     try {
-      const response = await axios.post(`${bvaiBaseUrl}/projects`, requestBody, {
-        headers: standardHeaders,
-      });
+      const response = await axios.post(
+        `${bvaiBaseUrl}/projects`,
+        requestBody,
+        {
+          headers: standardHeaders,
+        }
+      );
 
       if (response.status === 200) {
         resolve(response.data.data as ProjectWithSettings);
